b512fd5aa0847ebcff65adfa3e331000daa8f6b1
Team 1280 Programming Laptop <59985235+Team1280Programming@users.noreply.github.com>
2023-02-26 19:20:54 +0000

    Update arcade drive mode source

--- Differential/Modes.java
+++ Differential/Modes.java
@@ -178,17 +178,21 @@ public class Modes {
             }
         }
         public double[] set(double r, double theta, double[] state) {
-            // expects r in [0, 1] and theta measured from right horizontal in [0, 2pi)
-    
+            // expects r in [0, 1] and theta measured from right horizontal
+
+            // get robot pose (correct for field-oriented controls)
+            double pose = state[2] % (2 * Math.PI);
+            double nTheta = (2 * Math.PI + theta - pose + Math.PI / 2) % (2 * Math.PI);
+
             // check magnitude tolerance
             if (r < rEpsilon) {
                 return new double[] {0, 0};
             }
 
-            if (theta < Math.PI) {
-                return setTop(r, theta);
+            if (nTheta < Math.PI) {
+                return setTop(r, nTheta);
             } else {
-                double[] equivalents = setTop(r, 2 * Math.PI - theta);
+                double[] equivalents = setTop(r, 2 * Math.PI - nTheta);
                 double[] bottom = {-equivalents[0], -equivalents[1]};
                 return bottom;
             }
