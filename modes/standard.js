a9011fa22da95efc26ebbe2d9ba08a91f417b26a
Team 1280 Programming Laptop <59985235+Team1280Programming@users.noreply.github.com>
2023-03-17 16:43:58 -0700

    Fix robot-centric standard drive

--- modes/standard.js
+++ modes/standard.js
@@ -47,18 +47,16 @@ class StandardRobot {
             return [power, power]
         }
 
-        // choose between forwards and backwards movement
-        let target
-        if (dTheta < dThetaBackwards) {
-            target = theta
-        } else {
-            target = (theta + Math.PI) % (2 * Math.PI)
-        }
+        // constant target (never orient backwards)
+        let target = theta
 
         // find turning direction to target
-        const turnAngle = pose - target
-        const power = this.lerp(0, this.max, 2 * Math.abs(turnAngle) / Math.PI);
-        if (turnAngle < 0) {
+        const turnAngle = Math.abs(pose - target)
+        const power = this.lerp(0, this.max, 2 * turnAngle / Math.PI);
+        const verticalPose = (pose - 3 * Math.PI / 2 + 2 * Math.PI) % (2 * Math.PI)
+        const verticalTarget = (target - 3 * Math.PI / 2 + 2 * Math.PI) % (2 * Math.PI)
+        
+        if (verticalPose - verticalTarget < 0) {
             // left turn
             return [-power, power]
         } else {
